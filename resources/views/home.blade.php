44675c1c4eda524f7f87891bb94c38796eabbd71
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-03-03 18:34:33 +0100

    Add: merchbox

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,6 @@
 @extends('layouts.default')
+
+
+@section('main')
+@include("partials.merchbox")
+@endsection
---
+++ resources/views/partials/merchbox.blade.php
@@ -0,0 +1,32 @@
+<div class="merch-box txt-style">
+    <div class="card-buy-box">
+        <div class="box-img">
+            <img src="{{ asset('images/buy-comics-digital-comics.png') }}" alt="">
+        </div>
+        <div class="box-txt">DIGITAL COMICS</div>
+    </div>
+    <div class="card-buy-box">
+        <div class="box-img">
+            <img src="{{ asset('images/buy-comics-merchandise.png') }}" alt="">
+        </div>
+        <div class="box-txt">DC MERCHANDISE</div>
+    </div>
+    <div class="card-buy-box">
+        <div class="box-img">
+            <img src="{{ asset('images/buy-comics-subscriptions.png') }}" alt="">
+        </div>
+        <div class="box-txt">SUBSCRIPTION</div>
+    </div>
+    <div class="card-buy-box">
+        <div class="box-img">
+            <img src="{{ asset('images/buy-comics-shop-locator.png') }}" alt="">
+        </div>
+        <div class="box-txt">COMIC SHOP LOCATOR</div>
+    </div>
+    <div class="card-buy-box">
+        <div class="box-img">
+            <img src="{{ asset('images/buy-dc-power-visa.svg') }}" alt="">
+        </div>
+        <div class="box-txt">DC POWER VISA</div>
+    </div>
+</div>
