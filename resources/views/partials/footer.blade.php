92962d95a626d74e7a4b91eeb0609df73bb774e9
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-03-03 18:05:36 +0100

    Add: footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,50 @@
-footer
+<footer>
+    <div class="main-container">
+        <div class="box-lists">
+            <div class="txt-style">
+                <h4>DC COMICS</h4>
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>5</li>
+                    <li>6</li>
+                    <li>7</li>
+                </ul>
+                <h4>SHOP</h4>
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                </ul>
+            </div>
+            <div class="txt-style">
+                <h4>DC</h4>
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>5</li>
+                    <li>6</li>
+                    <li>7</li>
+                    <li>8</li>
+                    <li>9</li>
+                    <li>10</li>
+                    <li>11</li>
+                </ul>
+            </div>
+            <div class="txt-style">
+                <h4>SITES</h4>
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>5</li>
+                </ul>
+            </div>
+        </div>
+        <div class="box-logo-bg"></div>
+    </div>
+</footer>
