c072e57e93548410fd60d270a9bc3a8dbe928dbb
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-03-03 17:40:24 +0100

    Add: navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,1 +1,19 @@
-navbar
+<div class="nav-container">
+        <div class="logo">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="">
+        </div>
+        <nav>
+            <ul class="txt-style">
+                <li>CHARACTERS</li>
+                <li>COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTABLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
+        </nav>
+    </div>
