9944151b764fea5ab3cc813b9456856880bbfd7c
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-03-08 21:59:50 +0100

    Add: comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,14 @@
 
 
 @section('main')
+
+<div class="card-container">
+@foreach($comics as $comic)
+    <div class="card txt-style">  
+        <img src="{{$comic['thumb'] }}" alt="">   
+        <h4> {{ $comic['title'] }} </h4>
+    </div>
+@endforeach
+</div>
 @include("partials.merchbox")
 @endsection
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>@yield('pageTitle') | La Molisana</title>
+  <title>Home</title>
 
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
@@ -18,7 +18,7 @@
     @include("partials.navbar")
   </header>
   @include("partials.jumbotron")
-  <main>
+  <main class="bg-dark">
   @yield('main')
   </main>
 
